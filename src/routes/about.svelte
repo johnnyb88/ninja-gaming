1d6635ae6726b391f8376523de3d5b65261b6a1a
John Barratt <johnnybarratt@gmail.com>
2022-08-10 11:52:01 -0700

    add my initial files

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<div class="about">
+  <h2>About Us</h2>
+  <p>Lorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsf</p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+  
+</script>
+
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>Lorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsfLorem ipsum dolor sit amet, afdsafsda fsdfdsafsa, adsf afa dfadsfasfas, adf adsf dsf af. dsafdsafsad fasdfasd fasdfas, asdfsaf.
+    adsfasdfasfasfasddfasdfafafadf. sadf asdfLorddresdfa. dfeafadsf
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
