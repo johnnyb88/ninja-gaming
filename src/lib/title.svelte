110207ba5f543ef45249bf7f06572fa0464dcf1c
John Barratt <johnnybarratt@gmail.com>
2022-08-10 12:27:19 -0700

    create reusable title component with icon

---
+++ src/lib/title.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let title = "Ninja Gaming";
+
+</script>
+
+<div class="title">
+  <img src="/console.png" alt="site logo">
+  <h1>{title}</h1>
+</div>
+
+<style>
+  .title {
+    display: flex;
+  }
+  .title img {
+    display: inline-block;
+    filter: invert(88%) sepia(18%) saturate(886%) hue-rotate(199deg) brightness(100%) contrast(90%);
+    width: 70px;
+    height:auto;
+  }
+  .title h1 {
+    margin-left: 10px;
+  }
+
+</style>
