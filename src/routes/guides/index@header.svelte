f3ad808bddf0c49bfd5a781214e20087f5c7d203
John Barratt <johnnybarratt@gmail.com>
2022-08-10 19:05:21 -0700

    add sveltekit prefetch for guides

--- src/routes/guides/index@header.svelte
+++ src/routes/guides/index@header.svelte
@@ -26,7 +26,7 @@
 <div class="guides">
   <ul>
     {#each guides as guide}
-    <li><a href={`/guides/${guide.id}`}>{guide.title}</a></li>
+    <li><a sveltekit:prefetch href={`/guides/${guide.id}`}>{guide.title}</a></li>
     {/each}
   </ul>
 </div>
