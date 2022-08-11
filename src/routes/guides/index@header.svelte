bcfa8c3e21c1c04e4ec5ff019b323b454acb818c
John Barratt <johnnybarratt@gmail.com>
2022-08-10 17:44:33 -0700

    fetch guides from json placeholder with context module script

--- src/routes/guides/index@header.svelte
+++ src/routes/guides/index@header.svelte
@@ -1,9 +1,35 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const guides = await res.json();
+
+    if (res.ok) {
+      return {
+        props: {
+          guides
+        }
+      }
+    }
+
+    return {
+      status: res.status,
+      error: new Error('Could not fetch the guides')
+    }
+  }
+</script>
+
+<script>
+import { each } from "svelte/internal";
+
+
+  export let guides
+</script>
 
 <div class="guides">
   <ul>
-    <li><a href="/">guide 1</a></li>
-    <li><a href="/">guide 1</a></li>
-
+    {#each guides as guide}
+    <li><a href="/">{guide.title}</a></li>
+    {/each}
   </ul>
 </div>
 
